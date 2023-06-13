7c2ad8a6dfb3c4d076f3e3c26895325288964a99
MalenQuilla <tungdv.bi12-476@st.usth.edu.vn>
2023-06-13 23:25:29 +0700

    semi_final

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from queue import Queue, Empty
+from queue import Queue
 from tkinter import PhotoImage, Canvas, Tk, NW
 from math import sin, cos, radians
 from new_recorder import recorder
@@ -10,7 +10,168 @@ class GUI:
         self.__root.geometry('1920x1080')
         self.__root.attributes('-fullscreen', True)
         self.__root.resizable(width= False, height= False)    
+        
+        self.__tune = recorder()
+        
+        # color for background
+        self.__start_color = "#fc00ff" 
+        self.__end_color = "#00dbde"
 
+    def start_normal_mode(self):
+        try:
+            self.__frame.destroy()
+        except:
+            pass
+        
+        thread1 = Thread(target= run.create_background_4_normal_mode())
+        thread2 = Thread(target= run.normal_mode())
+        
+        thread1.start()
+        thread2.start()
+        
+    def start_advance_mode(self):
+        try:
+            self.__frame.destroy()
+        except:
+            pass
+    
+    def create_main_frame(self):
+        self.__main_frame = Canvas(self.__root, 
+                            width= 1920,
+                            height= 200,
+                            bg= 'black',
+                            borderwidth= 0,
+                            highlightthickness= 0)
+        self.__main_frame.pack()
+        
+        self.__exit_canvas = Canvas(self.__main_frame,
+                             width= 70, 
+                             height= 70, 
+                             borderwidth= 0, 
+                             highlightthickness= 0)
+        self.__exit_canvas.place(x= 1850, y= 0) 
+        
+        extra_dirs = ['_clicked', '_moused', '']
+        
+        advance_dirs, normal_dirs = [], []
+        
+        for i in extra_dirs:
+            advance_dirs.append('GUI/Advance' + i + '.png')
+            normal_dirs.append('GUI/Normal' + i + '.png')
+         
+        advance_pics, normal_pics = [], []
+        for i in range(3):  
+            advance_pics.append(PhotoImage(file= advance_dirs[i]))
+            normal_pics.append(PhotoImage(file= normal_dirs[i]))
+        
+        advance_canvas = Canvas(self.__main_frame, 
+                                width= 250, 
+                                height= 70,
+                                borderwidth= 0,
+                                highlightthickness= 0)
+        advance_canvas.place(x= 1024, y= 87)
+        
+        normal_canvas = Canvas(self.__main_frame, 
+                                width= 250, 
+                                height= 70,
+                                borderwidth= 0,
+                                highlightthickness= 0)
+        normal_canvas.place(x= 645, y= 87)
+        
+        for i in range(200):
+            r = int(self.__start_color[1:3], 16) + (int(self.__end_color[1:3], 16) - int(self.__start_color[1:3], 16)) * i // 1080
+            g = int(self.__start_color[3:5], 16) + (int(self.__end_color[3:5], 16) - int(self.__start_color[3:5], 16)) * i // 1080
+            b = int(self.__start_color[5:7], 16) + (int(self.__end_color[5:7], 16) - int(self.__start_color[5:7], 16)) * i // 1080
+            color = "#" + hex(r)[2:].zfill(2) + hex(g)[2:].zfill(2) + hex(b)[2:].zfill(2)
+
+            if i <= 69:
+                self.__main_frame.create_rectangle(0, i, 1850, i + 1, fill=color, outline="")
+                self.__exit_canvas.create_rectangle(0, i, 70, i + 1, fill=color, outline="")
+            elif i >= 87 and i <= 70 + 86:
+                self.__main_frame.create_rectangle(0, i, 645, i + 1, fill=color, outline="")
+                self.__main_frame.create_rectangle(895, i, 1024, i + 1, fill=color, outline="")
+                self.__main_frame.create_rectangle(1274, i, 1920, i + 1, fill=color, outline="")
+                
+                advance_canvas.create_rectangle(0, i - 87, 250, i - 86, fill=color, outline="")
+                normal_canvas.create_rectangle(0, i - 87, 250, i - 86, fill=color, outline="")
+            else:
+                self.__main_frame.create_rectangle(0, i, 1920, i + 1, fill=color, outline="") 
+        
+        self.__exit_pic = PhotoImage(file= 'GUI/exit.png')
+        
+        def exit_click(event):
+            self.__tune.stop_record = True
+            
+            self.__root.destroy()
+        
+        exit_button = self.__exit_canvas.create_image(0, 
+                                               0, 
+                                               anchor= NW, 
+                                               image= self.__exit_pic) 
+        
+        self.__exit_canvas.tag_bind(exit_button, '<Button-1>', exit_click) 
+        
+        
+        advance_button = advance_canvas.create_image(125, 
+                                               35, 
+                                               image= advance_pics[2]) 
+        advance_canvas.image = advance_pics[2]
+        
+        normal_button = normal_canvas.create_image(125, 
+                                               35, 
+                                               image= normal_pics[0])  
+        normal_canvas.image = normal_pics[0]
+        
+        def advance_mouse(event):
+            if not self.__tune.advanceMode:
+                advance_canvas.itemconfig(advance_button, image= advance_pics[1])
+                advance_canvas.image = advance_pics[1]
+        def advance_click(event):
+            if not self.__tune.advanceMode:
+                advance_canvas.itemconfig(advance_button, image= advance_pics[0])
+                advance_canvas.image = advance_pics[0]
+                
+                normal_canvas.itemconfig(normal_button, image= normal_pics[2])
+                normal_canvas.image = normal_pics[2]
+                
+                self.__tune.advanceMode = True
+                
+                self.start_advance_mode()
+        def advance_leave(event):
+            if not self.__tune.advanceMode:
+                advance_canvas.itemconfig(advance_button, image= advance_pics[2])
+                advance_canvas.image = advance_pics[2]
+        
+        advance_canvas.tag_bind(advance_button, '<Enter>', advance_mouse) 
+        advance_canvas.tag_bind(advance_button, '<Button-1>', advance_click) 
+        advance_canvas.tag_bind(advance_button, '<Leave>', advance_leave)
+        
+        
+        def normal_mouse(event):
+            if self.__tune.advanceMode:
+                normal_canvas.itemconfig(normal_button, image= normal_pics[1])
+                normal_canvas.image = normal_pics[1]
+        def normal_click(event):
+            if self.__tune.advanceMode:
+                normal_canvas.itemconfig(normal_button, image= normal_pics[0])
+                normal_canvas.image = normal_pics[0]
+                
+                advance_canvas.itemconfig(advance_button, image= advance_pics[2])
+                advance_canvas.image = advance_pics[2]
+                
+                self.__tune.advanceMode = False
+                self.start_normal_mode()
+        def normal_leave(event):
+            if self.__tune.advanceMode:
+                normal_canvas.itemconfig(normal_button, image= normal_pics[2])
+                normal_canvas.image = normal_pics[2]
+        
+        normal_canvas.tag_bind(normal_button, '<Button-1>', normal_click) 
+        normal_canvas.tag_bind(normal_button, '<Enter>', normal_mouse) 
+        normal_canvas.tag_bind(normal_button, '<Leave>', normal_leave)
+               
+        
+    
     def animate_bar_thread(self, target):
         Thread(target= lambda: self.animate_bar(target)).start()
         Thread(target= lambda: self.animate_bar(target)).start()
@@ -30,7 +191,7 @@ class GUI:
                     self.__angle -= 1
                     self.__root.after(1, lambda: self.animate_bar(target))
             return
-    
+     
     def create_background_4_normal_mode(self):
         self.__root.title('Normal mode')
         
@@ -45,35 +206,26 @@ class GUI:
             clicked_dirs.append('GUI/normal_mode/' + i + '_clicked.png')
         
         self.__pic_moused = (PhotoImage(file= moused_dirs[0]),
-                      PhotoImage(file= moused_dirs[1]),
-                      PhotoImage(file= moused_dirs[2]),
-                      PhotoImage(file= moused_dirs[3]),
-                      PhotoImage(file= moused_dirs[4]),
-                      PhotoImage(file= moused_dirs[5]))
-        
+                             PhotoImage(file= moused_dirs[1]),
+                             PhotoImage(file= moused_dirs[2]),
+                             PhotoImage(file= moused_dirs[3]),
+                             PhotoImage(file= moused_dirs[4]),
+                             PhotoImage(file= moused_dirs[5]))
+                
         self.__pic_clicked = (PhotoImage(file= clicked_dirs[0]),
-                      PhotoImage(file= clicked_dirs[1]),
-                      PhotoImage(file= clicked_dirs[2]),
-                      PhotoImage(file= clicked_dirs[3]),
-                      PhotoImage(file= clicked_dirs[4]),
-                      PhotoImage(file= clicked_dirs[5]))
+                              PhotoImage(file= clicked_dirs[1]),
+                              PhotoImage(file= clicked_dirs[2]),
+                              PhotoImage(file= clicked_dirs[3]),
+                              PhotoImage(file= clicked_dirs[4]),
+                              PhotoImage(file= clicked_dirs[5]))
         
         self.__frame = Canvas(self.__root, 
                        width= 1920, 
-                       height= 1080, 
+                       height= 880, 
                        bg= 'black', 
                        borderwidth= 0, 
                        highlightthickness= 0)
         self.__frame.pack() 
-                
-        self.__exit_pic = PhotoImage(file= 'GUI/exit.png')
-        
-        self.__exit_canvas = Canvas(self.__frame,
-                             width= 70, 
-                             height= 70, 
-                             borderwidth= 0, 
-                             highlightthickness= 0)
-        self.__exit_canvas.place(x= 1850, y= 0) 
         
         self.__speedometer = PhotoImage(file= 'GUI/normal_mode/speedometer.png')
         
@@ -83,26 +235,20 @@ class GUI:
                                    borderwidth= 0, 
                                    highlightthickness= 0,
                                    bg = 'white')
-        self.__speedometor_frame.place(x= 225, y= 340)
-        
-        start_color = "#fc00ff" 
-        end_color = "#00dbde"
-        
-        for i in range(1080):
-            r = int(start_color[1:3], 16) + (int(end_color[1:3], 16) - int(start_color[1:3], 16)) * i // 1080
-            g = int(start_color[3:5], 16) + (int(end_color[3:5], 16) - int(start_color[3:5], 16)) * i // 1080
-            b = int(start_color[5:7], 16) + (int(end_color[5:7], 16) - int(start_color[5:7], 16)) * i // 1080
+        self.__speedometor_frame.place(x= 225, y= 140)
+                
+        for i in range(200, 1080):
+            r = int(self.__start_color[1:3], 16) + (int(self.__end_color[1:3], 16) - int(self.__start_color[1:3], 16)) * i // 1080
+            g = int(self.__start_color[3:5], 16) + (int(self.__end_color[3:5], 16) - int(self.__start_color[3:5], 16)) * i // 1080
+            b = int(self.__start_color[5:7], 16) + (int(self.__end_color[5:7], 16) - int(self.__start_color[5:7], 16)) * i // 1080
             color = "#" + hex(r)[2:].zfill(2) + hex(g)[2:].zfill(2) + hex(b)[2:].zfill(2)
 
-            if i < 70:
-                self.__frame.create_rectangle(0, i, 1850, i + 1, fill=color, outline="")
-                self.__exit_canvas.create_rectangle(0, i, 70, i + 1, fill=color, outline="")
-            elif i >= 340:
-                self.__frame.create_rectangle(0, i, 225, i + 1, fill=color, outline="")
-                self.__frame.create_rectangle(225 + 1470, i, 1920, i + 1, fill=color, outline="")
+            if i >= 340:
+                self.__frame.create_rectangle(0, i - 200, 225, i - 199, fill=color, outline="")
+                self.__frame.create_rectangle(225 + 1470, i - 200, 1920, i - 199, fill=color, outline="")
                 self.__speedometor_frame.create_rectangle(0, i - 340, 1470, i - 339, fill=color, outline="")
             else:
-                self.__frame.create_rectangle(0, i, 1920, i + 1, fill=color, outline="")    
+                self.__frame.create_rectangle(0, i - 200, 1920, i - 199, fill=color, outline="")    
                 
         self.__speedometer_pic = self.__speedometor_frame.create_image(0, 
                                                          0, 
@@ -110,42 +256,28 @@ class GUI:
                                                          image= self.__speedometer)
      
      
-    def normal_mode(self):      
-        
-        def exit_click(event):
-            tune.stop_record = True
-            
-            self.__root.destroy()
-        
-        exit_button = self.__exit_canvas.create_image(0, 
-                                               0, 
-                                               anchor= NW, 
-                                               image= self.__exit_pic) 
-        
-        self.__exit_canvas.tag_bind(exit_button, '<Button-1>', exit_click)   
+    def normal_mode(self):        
                                 
         Note = [82.41, 110, 146.83, 196, 246.94, 329.63]
-        
-        tune = recorder()
         
         shared_queue = Queue()
         
         def chooseNote(note):
             def animatee():
-                target = round(tune.difference * (-2) + 90)  
+                target = round(self.__tune.difference * (-2) + 90)  
                                          
                 shared_queue.put(target)
-                    
-                print(tune.freq, target)
                 
                 target = shared_queue.get(timeout=1)  # Wait for a target value for up to 1 second
                 if target <= 290 and target >= 0 and self.__angle != target:
                     if target > 180:
                         target = 180
                     
+                    print(self.__tune.freq, target)
+                    
                     self.animate_bar_thread(target)
                 
-                if not tune.stop_record:
+                if not self.__tune.stop_record:
                     self.__root.after(200, animatee)
                 else:
                     self.animate_bar_thread(90)
@@ -153,7 +285,7 @@ class GUI:
                     
             
             def run_record():                    
-                tune.record(note)
+                self.__tune.record(note)
                             
             thread1 = Thread(target=run_record)
             thread2 = Thread(target=animatee)
@@ -207,7 +339,7 @@ class GUI:
                     
         def E2_click(event):
             if not self.__isClicked:
-                tune.stop_record = False
+                self.__tune.stop_record = False
                 self.__isClicked = True
                 chooseNote(Note[0])
                 self.__speedometor_frame.itemconfig(self.__speedometer_pic, image= self.__pic_clicked[0])
@@ -215,7 +347,7 @@ class GUI:
 
         def A2_click(event):
             if not self.__isClicked:
-                tune.stop_record = False
+                self.__tune.stop_record = False
                 self.__isClicked = True
                 chooseNote(Note[1])
                 self.__speedometor_frame.itemconfig(self.__speedometer_pic, image= self.__pic_clicked[1])
@@ -223,7 +355,7 @@ class GUI:
 
         def D3_click(event):
             if not self.__isClicked:
-                tune.stop_record = False
+                self.__tune.stop_record = False
                 self.__isClicked = True
                 chooseNote(Note[2])
                 self.__speedometor_frame.itemconfig(self.__speedometer_pic, image= self.__pic_clicked[2])
@@ -231,7 +363,7 @@ class GUI:
 
         def G3_click(event):
             if not self.__isClicked:
-                tune.stop_record = False
+                self.__tune.stop_record = False
                 self.__isClicked = True
                 chooseNote(Note[3])
                 self.__speedometor_frame.itemconfig(self.__speedometer_pic, image= self.__pic_clicked[3])
@@ -239,7 +371,7 @@ class GUI:
 
         def B3_click(event):
             if not self.__isClicked:
-                tune.stop_record = False
+                self.__tune.stop_record = False
                 self.__isClicked = True
                 chooseNote(Note[4])
                 self.__speedometor_frame.itemconfig(self.__speedometer_pic, image= self.__pic_clicked[4])
@@ -247,7 +379,7 @@ class GUI:
 
         def E4_click(event):
             if not self.__isClicked:
-                tune.stop_record = False
+                self.__tune.stop_record = False
                 self.__isClicked = True
                 chooseNote(Note[5])
                 self.__speedometor_frame.itemconfig(self.__speedometer_pic, image= self.__pic_clicked[5])
@@ -255,7 +387,7 @@ class GUI:
         
         def Unclick(event):
             if self.__isClicked:
-                tune.stop_record = True
+                self.__tune.stop_record = True
                 self.__isClicked = False
                 self.__speedometor_frame.itemconfig(self.__speedometer_pic, image= self.__speedometer)
                 self.__speedometor_frame.image = self.__speedometer
@@ -299,7 +431,7 @@ class GUI:
         self.__center_x = 735
         self.__center_y = 735
 
-        self.__bar_length = 380
+        self.__bar_length = 300
 
         self.__angle = 90
         
@@ -315,10 +447,11 @@ class GUI:
         
         self.__root.mainloop()
         
+        def create_background_4_advance_mode(self):
+            pass
+        
 run = GUI()
-
-thread1 = Thread(target= run.create_background_4_normal_mode())
-thread2 = Thread(target= run.normal_mode())
-
+thread1 = Thread(target= run.create_main_frame())
 thread1.start()
-thread2.start()
+
+run.start_normal_mode()
--- new_recorder.py
+++ new_recorder.py
@@ -1,8 +1,6 @@
-from numpy import log2, log10, floor, ceil, float32, zeros, cos, linspace, fromstring, int16, pi, fft
+from numpy import log2, floor, ceil, float32, zeros, cos, linspace, fromstring, int16, pi, fft
 from pyaudio import PyAudio, paInt16
-from time import time
-from tkinter import TclError
-from matplotlib import pyplot as plt
+
 
 class recorder:
     def __init__(self):
@@ -51,50 +49,21 @@ class recorder:
 
         # print ('sampling at', FSAMP, 'Hz with max resolution of', FREQ_STEP, 'Hz')
         # print()
-        
-        fig, ax = plt.subplots(figsize=(15, 7))
-        
-        frame_count = 0
-        start_time = time()
                 
         while stream.is_active():
             buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
             buf[-FRAME_SIZE:] = fromstring(stream.read(FRAME_SIZE), int16)
+            
             ffted = fft.rfft(buf * window)
             
             freq = (abs(ffted[imin:imax]).argmax() + imin) * FREQ_STEP
             
             num_frames += 1
             if num_frames >= FRAMES_PER_FFT:
-                if not self.advanceMode:
-                    self.freq =  freq
-                    
-                    self.difference = freq - note    
-                else:
-                    magnitude = abs(ffted)
-                    magnitude_db = 20 * log10(magnitude)
-                    
-                    spectrum, = ax.plot(magnitude_db)                               
-                    
-                    try:
-                        fig.canvas.draw()
-                        fig.canvas.flush_events()
-                        frame_count += 1
-                        
-                    except TclError:
-                        
-                        # calculate average frame rate
-                        frame_rate = frame_count / (time() - start_time)
-                        
-                        print('stream stopped')
-                        print('average frame rate = {:.0f} FPS'.format(frame_rate))
-                        break
+                self.freq =  freq
+                
+                self.difference = freq - note    
             
             if self.stop_record:
                 print('stopped')
                 return        
-            
-
-run = recorder()
-run.advanceMode = True
-run.record(110)
